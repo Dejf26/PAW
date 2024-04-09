e54a5c9bb4fec82115b4182e9de8b82a995356ee
Dawid Migdał <dejf026@gmail.com>
2024-04-09 13:03:01 +0200

    lab3 users done

--- PAW/src/interfaces/userInterface.ts
+++ PAW/src/interfaces/userInterface.ts
@@ -2,5 +2,11 @@ export interface User {
     id: number;
     firstName: string;
     lastName: string;
+    role: UserRole;
   }
   
+  export enum UserRole {
+    ADMIN = 'admin',
+    DEVOPS = 'devops',
+    DEVELOPER = 'developer'
+  }
--- PAW/src/user.ts
+++ PAW/src/user.ts
@@ -1,9 +1,12 @@
 // user.ts
-import { User as UserModel } from "./interfaces/userInterface";
+import { User as UserModel, UserRole } from "./interfaces/userInterface";
 
 export class User {
   private static instance: User | null = null;
-  private static readonly mockUser: UserModel = { id: 1, firstName: "John", lastName: "Doe" }; // Mock zalogowanego użytkownika
+  private static readonly mockAdminUser: UserModel = { id: 1, firstName: "John", lastName: "Doe", role: UserRole.ADMIN }; // Mock zalogowanego użytkownika z rolą admin
+  private static readonly mockDeveloperUser: UserModel = { id: 2, firstName: "Jane", lastName: "Smith", role: UserRole.DEVELOPER };
+  private static readonly mockDevOpsUser: UserModel = { id: 3, firstName: "Tom", lastName: "Brown", role: UserRole.DEVOPS };
+  private static readonly userList: UserModel[] = [User.mockAdminUser, User.mockDeveloperUser, User.mockDevOpsUser]; // Lista użytkowników
 
   private constructor() {}
 
@@ -14,7 +17,20 @@ export class User {
     return User.instance;
   }
 
+  getUsers(): UserModel[] {
+    return User.userList;
+  }
+
   getUser(): UserModel {
-    return User.mockUser;
+    return User.mockAdminUser;
+  }
+
+  setUserRole(role: UserRole): void {
+    User.mockAdminUser.role = role;
   }
 }
+
+
+
+// const user = User.getInstance();
+// user.setUserRole(UserRole.DEVELOPER); // Ustawienie roli użytkownika na developer
